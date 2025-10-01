5979f53e31c600e5f6aad4498916547b47d6686b
Mueller <ajdarn3@gmail.com>
2025-10-01 17:30:20 -0500

    Update env.js

--- env.js
+++ env.js
@@ -1,1 +1,4 @@
-window.ENV={SUPABASE_URL:'',SUPABASE_ANON_KEY:''};
+window.ENV = {
+  SUPABASE_URL: "https://https://fjshtoyqytvncsosgejg.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqc2h0b3lxeXR2bmNzb3NnZWpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMjExNzAsImV4cCI6MjA3NDg5NzE3MH0.DsEPymMXnKw_6eRXEZ-z7rEvjOIKN1YUslxT5DJbk2U"
+};
